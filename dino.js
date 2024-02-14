aba6950fb900a3d22cfac7826991eb5103871049
kojima yosuke <yabbee0828@gmail.com>
2024-02-14 16:15:37 +0900

    Mocked up

--- dino.js
+++ dino.js
@@ -23,6 +23,12 @@ let dino = {
 
 //cactus
 let cactusArray = [];
+let cactusId = 1;
+
+const obstacleTable = {};
+
+let jumpTicket = 2;
+// 地面についていたら、つまり初期のdinoYの一にいる場合は常にjumpTicketが2の状態になる感じ。一回jumpするごとにtikcetを切る。
 
 // さぼてんの横の大きさをここで定義している。
 let cactus1Width = 80;
@@ -83,10 +89,14 @@ window.onload = function () {
   // これら３つのfunctionを登録している。
   requestAnimationFrame(update);
   setInterval(placeCactus, 1000); //1000 milliseconds = 1 second
+  // 毎1秒ごとにcactusをpushしている、ただそれだけのことよね。そんで、lengthが5以上になったら最初のやつだけ取り除いている、っていうただそれだけよ。
+  // そうだから、arrayの中身は最初と全く変わるものになるっていうんだわな。。。どんどん古いものは捨てられていくってうことね。
   // document.addEventListener('keydown', moveDino); // 最初のコード
   // document.addEventListener('click', jumpDino); // 次、俺のjumpでのscreo更新
   // これ、今の状態だと、無限ジャンプできちゃうね。。。
   document.addEventListener('click', dodgeObstacle);
+  setInterval(seeScore, 100);
+  setInterval(isJumpAvailable, 100);
   // document.addEventListener('click', dodgeObstacle);
 };
 
@@ -110,7 +120,6 @@ function update() {
   context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
 
   //cactus
-  // console.log('arr ->', cactusArray);
   for (let i = 0; i < cactusArray.length; i++) {
     let cactus = cactusArray[i];
     cactus.x += velocityX;
@@ -126,7 +135,6 @@ function update() {
   }
 
   //score
-  // score += 10; // これだと、再描画のたびにscoreを更新し続けちゃうのか。。。。。
   context.fillStyle = 'black';
   context.font = '20px courier';
   context.fillText(`${score}p`, 5, 20);
@@ -156,12 +164,15 @@ function placeCactus() {
 
   //place cactus
   let cactus = {
+    id: cactusId,
     img: null,
     x: cactusX,
     y: cactusY,
     width: null,
     height: cactusHeight,
+    isDodged: false,
   };
+  cactusId += 1;
 
   //ここはそんな気にしなくてよくて、、、
   let placeCactusChance = Math.random(); // 0 - 0.9999...
@@ -181,13 +192,24 @@ function placeCactus() {
   }
 }
 
+// 要はぶつかったかどうかの判定をするコード。ぶつかっている状態ならgame overにするっていうロジック。
 function detectCollision(a, b) {
   return (
-    a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
-    a.x + a.width > b.x && //a's top right corner passes b's top left corner
-    a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
+    a.x < b.x + b.width && // a's top left corner doesn't reach b's top right corner
+    a.x + a.width > b.x && // a's top right corner passes b's top left corner
+    a.y < b.y + b.height && // a's top left corner doesn't reach b's bottom left corner
     a.y + a.height > b.y
-  ); //a's bottom left corner passes b's top left corner
+  ); // a's bottom left corner passes b's top left corner
+}
+
+function detectCollision(a, b) {
+  if (a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y) {
+    // ぶつかっています！！
+    return true;
+  } else {
+    if (!b.isDodged) {
+    }
+  }
 }
 
 // -----以下の俺のコード
@@ -195,22 +217,38 @@ function dodged(a, b) {}
 
 // jumpして避けるみたいな動作を加えようか。。。
 
+// jumpをしてobstacleを飛び越えたみたいな関数をどうしよう。。。
 function dodgeObstacle() {
+  if (jumpTicket === 0) {
+    return;
+  }
   velocityY = -10;
   let collision = false;
   // if(){} //サボテン一つに対してのぶつかったかぶつからなかったか判定する関数をここで実行していきたい。。。
-  console.log('array is this -> ', cactusArray);
+  // console.log('array is this -> ', cactusArray);
+  // つまりだ、、、古くて左にすでに流れているものに関してもjumpをやっちゃっているよね。。。つまり、自分よりすでに左にあるものに関しては何もやらなくていんじゃね？？？？そうだよな。。
   for (let i = 0; i < cactusArray.length; i++) {
+    // 毎回のjumpの度に自分より左にあるものを消すのか。。。。どうやろうか・・・
     let cactus = cactusArray[i];
-    if (detectCollision(dino, cactus)) {
-      collision = true;
-      break;
-    }
+    // if (isLocatedLeft(dino, cactus)) {
+    //   cactusArray.splice(i, 1);
+    //   console.log('after splice', cactusArray);
+    // }
+    // if(detectCollisionVer2(dino, cactus)){
+    //   console.log('logging')
+    // }
   }
-  console.log(collision);
-  if (!collision) {
-    score += 10;
+  jumpTicket -= 1;
+
+  console.log('jump rest', jumpTicket);
+  if (!jumpTicket) {
+    return;
+  } else {
   }
+  // console.log(collision);
+  // if (!collision) {
+  //   score += 10;
+  // }
 }
 
 // 障害物を避けたっていう判定が必要なんだよね。ここをどうするかなんだが、、、、
@@ -222,3 +260,41 @@ function jumpDino(e) {
 // もうさ、シンプルにarrayでもつのやめようかね。その代わり、
 // ballの種類を1 - 4つまでにしてrandom製にするかんじ。そっちの方が実装が楽かも。。。
 console.log('arr', cactusArray);
+
+// これで、飛び越えたかの判定をする。
+function detectCollisionVer2() {
+  return (
+    a.x > b.x + b.width && //a's top left corner doesn't reach b's top right corner
+    a.x + a.width < b.x && //a's top right corner passes b's top left corner
+    a.y > b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
+    a.y + a.height < b.y
+  );
+}
+
+// cactus arrayそれぞれに対してclick eventを発生させたい、ただそれだけよね。。
+function isLocatedLeft(a, b) {
+  return a.x > b.x + b.width;
+}
+
+// それより、arrayじゃなくてもう普通のobjectでいんじゃないかな。。。
+// それとはまた別で
+// arrayの状態を見るやつをまた別で置いておけばいいのか。。。
+function seeScore() {
+  for (let i = 0; i < cactusArray.length; i++) {
+    let cactus = cactusArray[i];
+    if (!obstacleTable[cactus.id]) {
+      if (isLocatedLeft(dino, cactus)) {
+        score += 10;
+        obstacleTable[cactus.id] = true;
+      }
+    } else {
+    }
+  }
+}
+
+// この感じで、jumpを監視するfunctionもあるといいのかもな。。。
+function isJumpAvailable() {
+  if (dino.y === dinoY) {
+    jumpTicket = 2;
+  }
+}
